f2033ff4837ed9e6f6745df36eb67770219cda2c
Martín0211 <martin021191@gmail.com>
2023-07-24 01:23:38 -0600

    Working

---
+++ client/src/view/index.js
@@ -0,0 +1,13 @@
+import Abaut from './Abaut/Abaut';
+import Detail from  './DetailPage/Detail';
+import Form from './Form/Form';
+import Home from './HomePage/Home';
+import Landing from './LandingPage/Landing';
+
+export {
+    Abaut,
+    Detail,
+    Form,
+    Home,
+    Landing,
+}
