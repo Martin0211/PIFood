eec5e504fe83db09516df327bbb8d06e321d07c8
Martín0211 <martin021191@gmail.com>
2023-07-24 00:46:45 -0600

    Controller running

--- api/src/controllers/controllerDiets.js
+++ api/src/controllers/controllerDiets.js
@@ -1,33 +1,37 @@
 const axios = require('axios');
-const { Diets, Recipe } = require('../db');
+const { Diets } = require('../db');
 const { Op } = require('sequelize');
 require('dotenv').config();
 const { YOUR_API_KEY } = process.env;
 
 const getAllDiets = async (req, res) => {
     try {
-
+        // Obtener todas las dietas almacenadas en la base de datos
         const diets = await Diets.findAll();
 
         if(diets.length === 0) {
-
+            // Si no hay dietas en la base de datos, obtenerlas de la API externa
+            
+            // Realizar una solicitud a la API para obtener información de recetas
             const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true`)
 
+            // Extraer las dietas únicas de los resultados de la API
             const apiDiets = response.data.results;
-            
-            // Obtener todas las dietas únicas
             const dietsUnique = Array.from(new Set(apiDiets.flatMap(recipe => recipe.diets)));
 
             // Guardar las dietas en la base de datos
             await Promise.all(dietsUnique.map(name => Diets.create({ name })));
             
+            // Devolver las dietas únicas como respuesta
             res.status(200).json(dietsUnique);
 
         } else {
+            // Si ya hay dietas en la base de datos, devolverlas como respuesta
             res.status(200).json(diets);
         }
 
     } catch (error) {
+        // En caso de error, devolver un mensaje de error
         console.error(error);
         res.status(500).json({message: 'Error al obtener las dietas.'});   
     }
--- api/src/controllers/controllerRecipe.js
+++ api/src/controllers/controllerRecipe.js
@@ -4,18 +4,36 @@ const { Op } = require('sequelize');
 require('dotenv').config();
 const { YOUR_API_KEY } = process.env;
 
+// Obtener una receta por su ID
 const getRecipeById = async (req, res) => {
-    const {idRecipe} = req.params;
+    const { idRecipe } = req.params;
     try {
-        const recipeDB = await Recipe.findByPk(idRecipe, {include: [Diets]});
-        if(recipeDB) {
-            res.status(200).json(recipeDB)
+        // Buscar la receta en la base de datos local
+        const recipeDB = await Recipe.findByPk(idRecipe, { include: [Diets] });
+
+        if (recipeDB) {
+            // Si se encuentra la receta en la base de datos, devolverla como respuesta
+            const diets = recipeDB.Diets.map(diet => diet.name);
+
+            const recipe = {
+                id: recipeDB.id,
+                title: recipeDB.title,
+                image: recipeDB.image,
+                summary: recipeDB.summary,
+                healthScore: recipeDB.healthScore,
+                instructions: recipeDB.instructions,
+                diets: diets,
+            };
+
+            res.status(200).json(recipe);
         } else {
+            // Si no se encuentra en la base de datos, obtenerla de la API externa
             const response = await axios.get(`https://api.spoonacular.com/recipes/${idRecipe}/information?apiKey=${YOUR_API_KEY}`);
             const recipeAPI = response.data;
 
+            // Crear un objeto con la información de la receta desde la API
             const recipe = {
-                id : recipeAPI.id,
+                id: recipeAPI.id,
                 title: recipeAPI.title,
                 image: recipeAPI.image,
                 summary: recipeAPI.summary,
@@ -23,93 +41,154 @@ const getRecipeById = async (req, res) => {
                 instructions: recipeAPI.instructions,
                 diets: recipeAPI.diets,
             }
+
+            // Devolver la receta como respuesta
             res.status(200).json(recipe);
         }
     } catch (error) {
+        // En caso de error, devolver un mensaje de error
         console.error(error);
-        res.status(500).json({ message: 'Error al obtener el receta por id' })
+        res.status(500).json({ message: 'Error al obtener la receta por id' });
     }
 }
 
-const getRecipeByName =async (req, res) => {
-
+// Obtener recetas por su nombre
+const getRecipeByName = async (req, res) => {
     try {
+        // Convertir el nombre de la receta a minúsculas
         const recipeName = req.query.title.toLowerCase();
+
+        // Buscar recetas en la base de datos local que coincidan con el nombre
         const recipeDB = await Recipe.findAll({
             where: {
                 title: {
                     [Op.iLike]: `%${recipeName}%`
                 }
-
             },
-            limit: 100
+            limit: 100,
+            include: [Diets],
         });
 
-        // Hacer la solicitud al endpoint que trae todas las recetas
-        const response = await axios.get(
-            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&number=100`
+        const mappedRecipes = recipeDB.map(recipe => ({
+            id: recipe.id,
+            title: recipe.title,
+            image: recipe.image,
+            summary: recipe.summary,
+            healthScore: recipe.healthScore,
+            instructions: recipe.instructions,
+            diets: recipe.Diets.map(diet => diet.name),
+        }));
+
+        // Hacer una solicitud a la API para obtener recetas que coincidan con el nombre
+        const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
+
+        // Filtrar las recetas de la API por nombre
+        const apiRecipe = response.data.results.filter((recipe) => 
+            recipe.title.toLowerCase().includes(recipeName)
         );
 
-        // Filtrar las recetas por nombre
-        
-        const apiRecipe = response.data.results.filter((recipe) => 
-        recipe.title.toLowerCase().includes(recipeName));
-        
-        if(apiRecipe.length === 0) {
-            return res.status(404).json({
-                mesage: 'No se encontraron recetas con ese nombre'
-            })
-        }
-
-        const allRecipe = [...recipeDB, ...apiRecipe];
-
-        if (allRecipe.length === 0) {
+        // Comprobar si se encontraron recetas en la base de datos o la API
+        if (apiRecipe.length === 0 && mappedRecipes.length === 0) {
             return res.status(404).json({
                 message: 'No se encontraron recetas con ese nombre'
             })
         }
-        
+
+        // Combinar las recetas de la base de datos y la API
+        const allRecipe = [...mappedRecipes, ...apiRecipe];
         res.status(200).json(allRecipe);
 
     } catch (error) {
+        // En caso de error, devolver un mensaje de error
         console.error(error);
-        res.status(500).json({message: 'Error al buscar las recetas por nombre'})
+        res.status(500).json({ message: 'Error al buscar las recetas por nombre' });
     }
 }
 
-const postNewRecipe = async (req, res) => {
+// Obtener todas las recetas por defecto, incluyendo las de la base de datos
+const getAllRecipes = async (req, res) => {
     try {
-        const{id, title, image, summary, healthScore, instructions, diets} = req.body;
-        const newRecipe = await Recipe.create({
-            id, 
-            title, 
-            image, 
-            summary, 
-            healthScore, 
-            instructions, 
+        // Hacer una solicitud a la API para obtener todas las recetas
+        const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
+
+        // Obtener las recetas de la respuesta de la API
+        const apiRecipe = response.data.results;
+
+        // Comprobar si se encontraron recetas en la API
+        if (apiRecipe.length === 0) {
+            return res.status(404).json({
+                message: 'No se encontraron recetas'
+            });
+        }
+
+        // Obtener todas las recetas almacenadas en la base de datos local
+        const recipeDB = await Recipe.findAll({
+            limit: 100,
+            include: [Diets],
         });
 
-        if(diets && diets.length > 0) {
+        const mappedRecipes = recipeDB.map(recipe => ({
+            id: recipe.id,
+            title: recipe.title,
+            image: recipe.image,
+            summary: recipe.summary,
+            healthScore: recipe.healthScore,
+            instructions: recipe.instructions,
+            diets: recipe.Diets.map(diet => diet.name),
+        }));
+
+        // Combinar las recetas de la base de datos y la API
+        const allRecipe = [...mappedRecipes, ...apiRecipe];
+        res.status(200).json(allRecipe);
+
+    } catch (error) {
+        // En caso de error, devolver un mensaje de error
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener todas las recetas' });
+    }
+}
+
+// Crear una nueva receta
+const postNewRecipe = async (req, res) => {
+    try {
+        // Obtener los datos de la receta del cuerpo de la solicitud
+        const { id, title, image, summary, healthScore, instructions, diets } = req.body;
+
+        // Crear una nueva receta en la base de datos
+        const newRecipe = await Recipe.create({
+            id,
+            title,
+            image,
+            summary,
+            healthScore,
+            instructions,
+        });
+
+        // Si se especificaron dietas, buscarlas en la base de datos
+        if (diets && diets.length > 0) {
             const foundDiets = await Diets.findAll({
                 where: {
                     name: diets,
                 },
             });
+
+            // Asociar las dietas encontradas a la nueva receta
             await newRecipe.addDiets(foundDiets);
         }
 
-        res.status(201).json({message: 'Receta creado exidotosamente.'})
+        // Devolver una respuesta exitosa
+        res.status(201).json({ message: 'Receta creada exitosamente.' });
 
     } catch (error) {
+        // En caso de error, devolver un mensaje de error
         console.error(error);
-        res.status(500).json({message: 'Error al crear el Receta.'});
+        res.status(500).json({ message: 'Error al crear la Receta.' });
     }
 }
-
-
 
 module.exports ={
     getRecipeById,
     getRecipeByName,
     postNewRecipe,
+    getAllRecipes
 }
