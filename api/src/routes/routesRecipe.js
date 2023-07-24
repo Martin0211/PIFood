ef419a8830ee9ec41952c947668c11a70867dd77
Martín0211 <martin021191@gmail.com>
2023-07-24 00:52:46 -0600

    Routes running

--- api/src/routes/routesRecipe.js
+++ api/src/routes/routesRecipe.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const router = Router();
-const {getRecipeById, getRecipeByName, postNewRecipe} = require('../controllers/controllerRecipe');
+const {getRecipeById, getRecipeByName, postNewRecipe, getAllRecipes} = require('../controllers/controllerRecipe');
 
 
 router.get('/recipes/name', getRecipeByName);
 router.get('/recipes/:idRecipe', getRecipeById);
+router.get('/allrecipes', getAllRecipes)
 router.post('/recipes', postNewRecipe)
 
 module.exports = router;
