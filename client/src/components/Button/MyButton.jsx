449915d2d217b6b1983fc141a28ced7904652add
Martín0211 <martin021191@gmail.com>
2023-07-24 00:56:28 -0600

    Working and stylish button

---
+++ client/src/components/Button/MyButton.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+
+const MyButtonStyled = styled.button`
+  border: 2px solid white;
+  background-color: transparent;
+  color: white;
+  padding: 1.5vw 6vw;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: white;
+    color: black;
+
+`
+
+const MyButton = ({ text, route }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(route);
+  };
+
+  return (
+    <MyButtonStyled onClick={handleClick}>{text}</MyButtonStyled>
+  )
+}
+
+export default MyButton;
